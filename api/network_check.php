8d61409f4555a659b4ddb350efd97de5de5f529b
Nathan Felix <natholas@gmail.com>
2016-10-09 16:05:13 +0200

    Added network_check

---
+++ api/network_check.php
@@ -0,0 +1,8 @@
+<?php
+
+$raw = shell_exec("cat /proc/net/dev");
+$numbers = explode("\n",$raw)[2];
+$numbers = explode(":  ",$numbers)[1];
+$numbers = explode("    ",$numbers)[0];
+
+echo $numbers;
